70dfc5172203244d676fa97ad900c72bad33d2cb
gushakov <Georgy.Ushakov@unil.ch>
2019-05-06 16:24:40 +0200

    patch

--- src/main/java/ch/unil/zkredis/HomeVm.java
+++ src/main/java/ch/unil/zkredis/HomeVm.java
@@ -1,9 +1,32 @@
 package ch.unil.zkredis;
 
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.zk.ui.Sessions;
+
+import javax.servlet.http.HttpSession;
 import java.io.Serializable;
+import java.util.Date;
 
 public class HomeVm implements Serializable {
 
     private static final long serialVersionUID = 6551060335946014151L;
 
+    // will be stored in session
+    private String storeInSession;
+
+    @Init
+    public void onInit() {
+        String now = new Date().toString();
+        this.storeInSession = now;
+        HttpSession nativeSession = (HttpSession) Sessions.getCurrent().getNativeSession();
+        System.out.println("Initializing viewModel, now: " +
+                now +
+                ", session ID: " + nativeSession.getId());
+    }
+
+    @Command
+    public void checkSession() {
+        System.out.println("Got from session: " + storeInSession);
+    }
 }
--- src/main/java/ch/unil/zkredis/ZkRedisBootApplication.java
+++ src/main/java/ch/unil/zkredis/ZkRedisBootApplication.java
@@ -10,4 +10,5 @@ public class ZkRedisBootApplication {
         SpringApplication.run(ZkRedisBootApplication.class);
     }
 
+
 }
